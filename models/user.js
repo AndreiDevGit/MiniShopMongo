169555b9fc597aca9c6fe51d33374cb4a0a3afa3
AndreiDev <a.v.babici@gmail.com>
2024-07-25 17:15:20 +0300

    add displaying the Cart Items

--- models/user.js
+++ models/user.js
@@ -36,12 +36,13 @@ class User {
     const updatedCart = {
       items: updatedCartItems
     }
-    
+
     const db = getDb()
     return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } })
 
 
   }
+
 
   getCart() {
     const db = getDb()
